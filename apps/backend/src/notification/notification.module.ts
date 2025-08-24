996a4139be7d43478512cf7b92cd205bd686482b
w0uldy0u <spacekim01@gmail.com>
2025-08-24 10:38:58 +0000

    chore(be): add notificationListener

--- apps/backend/src/notification/notification.module.ts
+++ apps/backend/src/notification/notification.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
+import { NotificationListener } from './notification.listener';
 
 @Module({
   controllers: [NotificationController],
-  providers: [NotificationService],
+  providers: [NotificationService, NotificationListener],
 })
 export class NotificationModule {}
