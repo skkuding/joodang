1df3996dee6a3bd114f791e5cdd96716d54f65ed
hyuntori <hyuntori@hyuntori.local>
2025-08-24 14:35:51 +0900

    feat(fe): gnb 완성

--- apps/frontend/src/app/(components)/footer.tsx
+++ apps/frontend/src/app/(components)/footer.tsx
@@ -12,32 +12,43 @@ export default function Footer() {
 
   return (
     <>
-      <div className="flex flex-row justify-center bg-white h-20">
-        <div>
-          {page === 'home' ? (
-            <>
-              <Image src={BlackHouse} alt="검은 집" width={32} height={32} />
-              <p className="text-black">홈</p>
-            </>
-          ) : (
-            <>
-              <Image src={GrayHouse} alt="회색 집" width={32} height={32} />
-              <p className="gray">주점 찾기</p>
-            </>
-          )}
-        </div>
-        <div>
-          {page === 'beer' ? (
-            <>
-              <Image src={BlackBeer} alt="검은 맥주" width={32} height={32} />
-              <p>주점 찾기</p>
-            </>
-          ) : (
-            <>
-              <Image src={GrayBeer} alt="회색 맥주" width={32} height={32} />
-              <p className="text-[#9B9B9B]">주점 찾기</p>
-            </>
-          )}
+      <div
+        className="bg-white h-20 flex items-center justify-center"
+        style={{ boxShadow: '0 -2px 8px rgba(0,0,0,0.1)' }}
+      >
+        <div className="flex flex-row justify-center gap-[60px] -mt-2">
+          <div
+            onClick={() => setPage('home')}
+            className="w-[60px] h-[54px] flex flex-col items-center"
+          >
+            {page === 'home' ? (
+              <>
+                <Image src={BlackHouse} alt="검은 집" width={32} height={32} />
+                <p className="text-black ">홈</p>
+              </>
+            ) : (
+              <>
+                <Image src={GrayHouse} alt="회색 집" width={32} height={32} />
+                <p className="text-[#9B9B9B]">홈</p>
+              </>
+            )}
+          </div>
+          <div
+            onClick={() => setPage('beer')}
+            className="w-[60px] h-[54px] flex flex-col items-center"
+          >
+            {page === 'beer' ? (
+              <>
+                <Image src={BlackBeer} alt="검은 맥주" width={32} height={32} />
+                <p className="text-black ">주점 찾기</p>
+              </>
+            ) : (
+              <>
+                <Image src={GrayBeer} alt="회색 맥주" width={32} height={32} />
+                <p className="text-[#9B9B9B]">주점 찾기</p>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </>
