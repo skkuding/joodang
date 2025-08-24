81790b93ee8c50c5fb1295c5586df02d59832478
mingyuseong <mingyu27@naver.com>
2025-08-24 17:21:45 +0900

    refactor(fe): separate routing

--- apps/frontend/src/app/(main)/layout.tsx
+++ apps/frontend/src/app/(main)/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
-import "./globals.css";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import "../globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +34,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased pt-[48px] pb-20`}
       >
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
