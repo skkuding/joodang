daedc6551bc7500437a8f3e9b71e44aa1b7d0c36
w0uldy0u <spacekim01@gmail.com>
2025-08-24 13:26:16 +0900

    feat(be): initialize prisma and update eslint rules

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,9 +5,16 @@ import { StoreModule } from './store/store.module';
 import { ReservationModule } from './reservation/reservation.module';
 import { NotificationModule } from './notification/notification.module';
 import { MenuModule } from './menu/menu.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [StoreModule, ReservationModule, NotificationModule, MenuModule],
+  imports: [
+    StoreModule,
+    ReservationModule,
+    NotificationModule,
+    MenuModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
