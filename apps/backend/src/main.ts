b08c25b7c7fd662b0b4d1b982af1718d74f13302
w0uldy0u <spacekim01@gmail.com>
2025-08-31 23:13:10 +0900

    chore(be): modify CORS

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,14 +5,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin: [
-    'http://localhost:3000',
-    'https://joodang-frontend.vercel.app',
-  ],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  allowedHeaders: ['Content-Type'], // ← JSON이면 이거 필요
-  // credentials: true, // 쿠키 안 쓰면 빼도 OK
-});
+    origin: [
+      'http://localhost:3000',
+      'https://joodang-frontend.vercel.app',
+      'https://joodang.com',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
