651399ae19847721d78c5567b7cdb3cb03a18bfe
mingyuseong <mingyu27@naver.com>
2025-09-03 20:21:39 +0900

    feat(fe): add prettier tailwind plugin

--- apps/frontend/app/components/Header.tsx
+++ apps/frontend/app/components/Header.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { IoIosArrowDown } from "react-icons/io";
 import {
   DropdownMenu,
@@ -9,9 +8,9 @@ import {
 
 export function Header() {
   return (
-    <div className="fixed left-0 right-0 top-0 w-full px-5 flex bg-white h-[68px] items-center ">
+    <div className="fixed left-0 right-0 top-0 flex h-[78px] w-full items-center bg-white px-5">
       <DropdownMenu>
-        <DropdownMenuTrigger className="flex gap-1 items-center">
+        <DropdownMenuTrigger className="flex items-center gap-1">
           <span className="text-xl font-semibold">성균관대학교</span>
           <IoIosArrowDown className="h-4 w-4" />
         </DropdownMenuTrigger>
