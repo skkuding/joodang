1971106cd4279abcf017da369014261d1a1cead5
w0uldy0u <spacekim01@gmail.com>
2025-09-04 04:50:08 +0900

    chore(fe): add apple-touch-icon

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <link
           rel="apple-touch-startup-image"
