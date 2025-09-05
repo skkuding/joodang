2780f72219282928e9536b268f2bf9c134ac0346
jwoojin9 <jwoojin9@naver.com>
2025-09-06 04:53:08 +0900

    feat: make enter store info page

--- apps/frontend/app/stores/createStore.ts
+++ apps/frontend/app/stores/createStore.ts
@@ -1,51 +1,53 @@
-import { create } from 'zustand'
-import { Menu, TimeSlot } from '../type'
+import { create } from "zustand";
+import { Menu, TimeSlot } from "../type";
 
 interface FormData {
-  name: string
-  phone: string
-  description: string
-  organizer: string
-  instagramId: string
-  startTime: string
-  endTime: string
-  reservationFee: number
-  college: string
-  bankCode: string
-  accountNumber: string
-  accountHolder: string
-  location: string
-  latitude: number
-  longitude: number
-  menus: Menu[]
-  timeSlots: TimeSlot[]
+  name: string;
+  description: string;
+  organizer: string;
+  startTime: string;
+  endTime: string;
+  reservationFee: number;
+  college: string;
+  icon: number;
+  totalCapacity: number;
+  contactInfo: string;
+  bankCode: string;
+  accountNumber: string;
+  accountHolder: string;
+  location: string;
+  latitude: number;
+  longitude: number;
+  menus: Menu[];
+  timeSlots: TimeSlot[];
 }
 
 interface CreateStoreStore {
-  modalPage: number
-  formData: FormData
-  setModalPage: (page: number) => void
-  setFormData: (data: FormData) => void
-  nextModal: () => void
-  backModal: () => void
+  modalPage: number;
+  formData: FormData;
+  setModalPage: (page: number) => void;
+  setFormData: (data: FormData) => void;
+  nextModal: () => void;
+  backModal: () => void;
 }
 
-export const useCreateStoreStore = create<CreateStoreStore>((set) => ({
+export const useCreateStoreStore = create<CreateStoreStore>(set => ({
   modalPage: 0,
   formData: {
-    name: '',
-    phone: '',
-    description: '',
-    organizer: '',
-    instagramId: '',
-    startTime: '',
-    endTime: '',
+    name: "",
+    description: "",
+    organizer: "",
+    startTime: "",
+    endTime: "",
     reservationFee: 0,
-    college: '',
-    bankCode: '',
-    accountNumber: '',
-    accountHolder: '',
-    location: '',
+    college: "",
+    icon: 0,
+    totalCapacity: 0,
+    contactInfo: "",
+    bankCode: "",
+    accountNumber: "",
+    accountHolder: "",
+    location: "",
     latitude: 0,
     longitude: 0,
     menus: [],
@@ -55,10 +57,10 @@ export const useCreateStoreStore = create<CreateStoreStore>((set) => ({
   setFormData: (data: FormData) => set({ formData: data }),
   nextModal: () =>
     set((state: { modalPage: number }) => ({
-      modalPage: state.modalPage + 1
+      modalPage: state.modalPage + 1,
     })),
   backModal: () =>
     set((state: { modalPage: number }) => ({
-      modalPage: state.modalPage - 1
-    }))
-}))
+      modalPage: state.modalPage - 1,
+    })),
+}));
