69a6133522d89aea74c81c6dc2260b6f0777cf60
hyuntori <hyuntori@hyuntori.local>
2025-09-07 16:01:38 +0900

    feat(fe): make cusion element for slider

--- apps/frontend/ui/slider.tsx
+++ apps/frontend/ui/slider.tsx
@@ -20,7 +20,7 @@ function Slider({
         : Array.isArray(defaultValue)
           ? defaultValue
           : [min, max],
-    [value, defaultValue, min, max],
+    [value, defaultValue, min, max]
   );
 
   return (
@@ -31,21 +31,21 @@ function Slider({
       min={min}
       max={max}
       className={cn(
-        "relative flex w-full touch-none items-center select-none data-[disabled]:opacity-50 data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col",
-        className,
+        "relative flex w-full touch-none select-none items-center data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col data-[disabled]:opacity-50",
+        className
       )}
       {...props}
     >
       <SliderPrimitive.Track
         data-slot="slider-track"
         className={cn(
-          "bg-[#D7D7D7] relative grow overflow-hidden rounded-full data-[orientation=horizontal]:h-1.5 data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-1.5",
+          "relative grow overflow-hidden rounded-full bg-[#D7D7D7] data-[orientation=horizontal]:h-1.5 data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-1.5"
         )}
       >
         <SliderPrimitive.Range
           data-slot="slider-range"
           className={cn(
-            "bg-[#FF5940] absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full",
+            "absolute bg-[#FF5940] data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full"
           )}
         />
       </SliderPrimitive.Track>
@@ -53,7 +53,10 @@ function Slider({
         <SliderPrimitive.Thumb
           data-slot="slider-thumb"
           key={index}
-          className="after:inset-[-8px] bg-color-common-100 ring-ring/50 block size-4 shrink-0 rounded-full border border-[#FF5940] shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50"
+          className={cn(
+            "bg-color-common-100 ring-ring/50 focus-visible:outline-hidden block size-4 shrink-0 rounded-full border border-[#FF5940] shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 disabled:pointer-events-none disabled:opacity-50",
+            "after:pointer-events-auto after:absolute after:-inset-4 after:rounded-full after:bg-transparent after:content-['']"
+          )}
         />
       ))}
     </SliderPrimitive.Root>
