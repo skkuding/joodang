2300c62747535e3882b45bfdb07a89e0d17657f5
mingyuseong <mingyu27@naver.com>
2025-09-08 13:06:59 +0900

    fix(fe): update main layout

--- apps/frontend/app/(main)/management/store/page.tsx
+++ apps/frontend/app/(main)/management/store/page.tsx
@@ -1,3 +1,11 @@
+import storeIcon from "@/public/icons/icon_orange_house.svg";
+import Image from "next/image";
 export default function Page() {
-  return <div>Page</div>;
+  return (
+    <div>
+      <div className="flex flex-col px-5 py-[30px]">
+        <Image src={storeIcon} alt="Store Icon" width={32} height={32} />
+      </div>
+    </div>
+  );
 }
