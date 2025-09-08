dcfc664ff601ab2fdf48747ab76ba1e5e3a29dd5
w0uldy0u <spacekim01@gmail.com>
2025-09-08 22:24:36 +0900

    fix(fe): toss deeplink

--- apps/frontend/app/(detail)/[storeId]/reservation/success/page.tsx
+++ apps/frontend/app/(detail)/[storeId]/reservation/success/page.tsx
@@ -23,14 +23,10 @@ export default function Page() {
     const storedReservationData = sessionStorage.getItem("reservationData");
     if (storedReservationData) {
       setReservationData(JSON.parse(storedReservationData));
-      // 사용 후 제거 (선택사항)
-      sessionStorage.removeItem("reservationData");
     }
     const storedStoreData = sessionStorage.getItem("storeData");
     if (storedStoreData) {
       setStoreData(JSON.parse(storedStoreData));
-      // 사용 후 제거 (선택사항)
-      sessionStorage.removeItem("storeData");
 
       if (storeData?.reservationFee && reservationData?.headcount) {
         setAmount(storeData?.reservationFee * reservationData?.headcount);
@@ -61,7 +57,7 @@ export default function Page() {
 
     const handleKakaoPayment = () => {
       // 카카오페이 앱 스킴 URL
-      const kakaoPayUrl = "kakaotalk://kakaopay";
+      const kakaoPayUrl = `kakaopay://money/to/bank?bank_code=${storeData?.bankCode ?? ""}&bank_account_number=${storeData?.accountNumber ?? ""}&amount=${amount ?? ""}`;
       // 웹 버전 URL
       const kakaoWebUrl = "https://pay.kakao.com";
 
