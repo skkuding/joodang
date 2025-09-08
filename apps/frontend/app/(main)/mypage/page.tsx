12ccc3afb552bd282ead0496c05edc3eb47b3fa2
mingyuseong <mingyu27@naver.com>
2025-09-08 17:03:58 +0900

    feat(fe): update auth sheet to cover non automatically opened situation

--- apps/frontend/app/(main)/mypage/page.tsx
+++ apps/frontend/app/(main)/mypage/page.tsx
@@ -1,38 +1,43 @@
 "use client";
 
 import { AuthSheet } from "@/app/components/AuthSheet";
+import { User } from "@/app/type";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { safeFetcher } from "@/lib/utils";
+import arrowIcon from "@/public/icons/icon_arrow.svg";
+import defaultProfileIcon from "@/public/icons/icon_default_profile.svg";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
-interface UserRole {
-  role: string;
-}
-
 export default function Page() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [userRole, setUserRole] = useState<string | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [openAuthSheet, setOpenAuthSheet] = useState(false);
   const [showApplyForm, setShowApplyForm] = useState(false);
   const [phone, setPhone] = useState("");
   const [organizer, setOrganizer] = useState("");
   const [isApplying, setIsApplying] = useState(false);
 
   useEffect(() => {
-    const checkAuth = async () => {
+    const fetchUser = async () => {
       try {
-        const roleData: UserRole = await safeFetcher.get("user/me/role").json();
-        setIsAuthenticated(true);
-        setUserRole(roleData.role);
-      } catch {
-        setIsAuthenticated(false);
-        setUserRole(null);
-      }
-    };
+        const user: User = await safeFetcher.get("user/me").json();
+        setUser(user);
+      } catch {}
 
-    checkAuth();
+      fetchUser();
+    };
   }, []);
+
+  const handleAuth = () => {
+    if (user) {
+      safeFetcher.post("auth/logout").then(() => {
+        setUser(null);
+        window.location.reload();
+      });
+    } else {
+      setOpenAuthSheet(true);
+    }
+  };
 
   const handleApplyForOwner = async () => {
     if (!phone || !organizer) {
@@ -65,22 +70,36 @@ export default function Page() {
     }
   };
 
-  if (!isAuthenticated) {
-    return (
-      <div>
-        <AuthSheet />
-      </div>
-    );
-  }
-
   return (
-    <div className="space-y-6 px-6 py-14">
-      <div className="text-center">
-        <h1 className="mb-2 text-2xl font-bold">마이페이지</h1>
-        <p className="text-gray-600">현재 권한: {userRole}</p>
+    <div className="flex flex-col bg-amber-100">
+      <AuthSheet
+        open={openAuthSheet}
+        onOpenChange={setOpenAuthSheet}
+        closeToHistory={false}
+      />
+      <div className="flex gap-3 bg-amber-200 p-5">
+        <Image src={defaultProfileIcon} alt="Default Profile" />
+        <div className="flex w-full flex-col">
+          <div className="flex justify-between">
+            <span className="text-xl font-semibold">
+              {user ? user.name : "로그인하세요"}
+            </span>
+            <div className="flex items-center gap-[2px]">
+              <Button variant={"ghost"} className="h-3" onClick={handleAuth}>
+                <span className="text-color-neutral-40 text-xs font-normal">
+                  {user ? "로그아웃" : "로그인"}
+                </span>
+                <Image src={arrowIcon} alt="Arrow Icon" width={15} />
+              </Button>
+            </div>
+          </div>
+          <span className="text-color-neutral-60 text-sm font-normal">
+            비회원 상태
+          </span>
+        </div>
       </div>
 
-      {userRole === "USER" && (
+      {/* {userRole === "USER" && (
         <div className="mx-auto max-w-md space-y-4">
           <div className="text-center">
             <h2 className="mb-2 text-lg font-semibold">관리자 권한 요청</h2>
@@ -151,7 +170,7 @@ export default function Page() {
             주점 등록하기
           </Button>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
