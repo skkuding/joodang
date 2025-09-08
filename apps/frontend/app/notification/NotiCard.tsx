678bffb063eb81d19f1b95eb898af0246858f0be
Clover229 <jeonsumin2002@gmail.com>
2025-09-08 23:35:05 +0900

    noti 컴포넌트 만듦

---
+++ apps/frontend/app/notification/NotiCard.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+import type { Notification } from "../type";
+import Link from "next/link";
+
+interface NotiCardProps {
+  noti: Notification;
+}
+export default function NotiCard({ noti }: NotiCardProps) {
+  return (
+    <div
+      key={noti.id}
+      className="w-[335px] rounded-2xl border-0 bg-white p-4 shadow-lg"
+    >
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <Image
+            src="/icons/icon_orange_noti.svg"
+            alt="Notification Icon"
+            width={24}
+            height={24}
+          />
+          <h2 className="max-w-[180px] text-base font-semibold text-red-500">
+            {noti.message}
+          </h2>
+        </div>
+        <Link href={noti.url} passHref>
+          <button className="flex text-xs text-gray-500 hover:underline">
+            <p>상세보기 </p>
+            <Image src="/icons/icon_arrow.svg" alt=">" width={14} height={14} />
+          </button>
+        </Link>
+      </div>
+
+      {noti.type !== "Reservation" ? (
+        <div className="mt-2 flex flex-col gap-2 whitespace-pre-line rounded-md bg-[#f5f5f5] p-2 text-sm text-gray-700">
+          <div className="flex justify-between">
+            <div className="flex gap-1">
+              <Image
+                src="icons/icon_gray_coloredHouse.svg"
+                alt="Store Icon"
+                width={16}
+                height={16}
+                className="mr-1 inline-block"
+              />
+              <p>주점명</p>
+            </div>
+            <p>{noti.title}</p>
+          </div>
+          {/* <div className="flex justify-between">
+                  <div className="flex gap-1">
+                    <Image
+                      src="icons/icon_gray_location.svg"
+                      alt="Location Icon"
+                      width={16}
+                      height={16}
+                      className="mr-1 inline-block"
+                    />
+                    <p>위치</p>
+                  </div>
+                  <p>{noti.location}</p>
+                </div> */}
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
--- apps/frontend/app/notification/page.tsx
+++ apps/frontend/app/notification/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { safeFetcher } from "@/lib/utils";
 import type { Notification } from "../type";
 import { AuthSheet } from "../components/AuthSheet";
+import NotiCard from "./NotiCard";
 
 export default async function NotificationPage() {
   const cookieStore = await cookies();
@@ -42,68 +43,7 @@ export default async function NotificationPage() {
       ) : (
         <div className="mt-[30px] flex flex-col items-center space-y-3 p-5">
           {data.map(item => (
-            <div
-              key={item.id}
-              className="w-[335px] rounded-2xl border-0 bg-white p-4 shadow-lg"
-            >
-              <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <Image
-                    src="/icons/icon_orange_noti.svg"
-                    alt="Notification Icon"
-                    width={24}
-                    height={24}
-                  />
-                  <h2 className="max-w-[180px] text-base font-semibold text-red-500">
-                    {item.message}
-                  </h2>
-                </div>
-                <Link href={item.url} passHref>
-                  <button className="flex text-xs text-gray-500 hover:underline">
-                    <p>상세보기 </p>
-                    <Image
-                      src="/icons/icon_arrow.svg"
-                      alt=">"
-                      width={14}
-                      height={14}
-                    />
-                  </button>
-                </Link>
-              </div>
-
-              {item.type !== "Reservation" ? (
-                <div className="mt-2 flex flex-col gap-2 whitespace-pre-line rounded-md bg-[#f5f5f5] p-2 text-sm text-gray-700">
-                  <div className="flex justify-between">
-                    <div className="flex gap-1">
-                      <Image
-                        src="icons/icon_gray_coloredHouse.svg"
-                        alt="Store Icon"
-                        width={16}
-                        height={16}
-                        className="mr-1 inline-block"
-                      />
-                      <p>주점명</p>
-                    </div>
-                    <p>{item.title}</p>
-                  </div>
-                  {/* <div className="flex justify-between">
-                  <div className="flex gap-1">
-                    <Image
-                      src="icons/icon_gray_location.svg"
-                      alt="Location Icon"
-                      width={16}
-                      height={16}
-                      className="mr-1 inline-block"
-                    />
-                    <p>위치</p>
-                  </div>
-                  <p>{item.location}</p>
-                </div> */}
-                </div>
-              ) : (
-                ""
-              )}
-            </div>
+            <NotiCard key={item.id} noti={item} />
           ))}
         </div>
       )}
