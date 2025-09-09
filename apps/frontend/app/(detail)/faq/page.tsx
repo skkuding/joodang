ba71126d5d567aa97e7b519b7b27b159bbfaf28c
mingyuseong <mingyu27@naver.com>
2025-09-09 19:19:07 +0900

    fix(fe): update height

--- apps/frontend/app/(detail)/faq/page.tsx
+++ apps/frontend/app/(detail)/faq/page.tsx
@@ -1,7 +1,12 @@
 import { Separator } from "@/app/(main)/components/Separator";
+import { FormSection } from "@/app/components/FormSection";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
-import cautionIcon from "@/public/icons/icon_gray_caution.svg";
-import Image from "next/image";
 
 export default function Page() {
   function FAQHeader() {
@@ -22,11 +27,17 @@ export default function Page() {
 
   function FAQList() {
     return (
-      <div className="flex flex-col items-center px-5 py-[30px]">
-        <Image src={cautionIcon} alt="caution" />
-        <p className="text-color-neutral-70 text-sm font-medium">
-          등록된 질문이 없어요
-        </p>
+      <div className="flex flex-col items-center bg-amber-100 px-5 py-[30px]">
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>
+              <FormSection title={"이러이러한 경우는 어떻게 하나요?"} />
+            </AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
     );
   }
